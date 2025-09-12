49c6b98b70eb6ae6eaa5489092be52502095d078
mungaiHub <amosmungai085@gmail.com>
2025-09-12 18:50:30 +0300

    introduction to streamlit

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Hi! i am the streamlit web app")
+st.header("i am the header")
+st.subheader("Hi i am the subheader")
+st.text("Hi i am the text function programmers uses inplace of paragraph tag")
+
+
+st.markdown("# Heading 1")
+st.markdown("## Heading 2")
+st.markdown("### Heading 3")
+st.markdown("> 💡 Pro Tip: Use filters on the sidebar to refine your data analysis.") #blockquotes
+st.markdown("![Alt Text](https://streamlit.io/images/brand/streamlit-mark-color.png)") #to add an image
+st.markdown("This is **bold**, this is *italic*, and this is ~~strikethrough~~.")
+st.markdown("You can also [add links](https://streamlit.io).")
+
