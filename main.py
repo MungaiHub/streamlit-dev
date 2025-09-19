7423fd812c93e829d2189b0a516e65880b73c788
mungaiHub <amosmungai085@gmail.com>
2025-09-19 17:33:24 +0300

    learn about graphs and sidebar

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import time as ts
 from datetime import time
+from matplotlib import pyplot as plt
+import numpy as np
+x=np.linspace(0,10,100)
+bar_x=np.array([1,2,3,4,5])
 
 # st.title("Hi! i am the streamlit web app")
 # st.header("i am the header")
@@ -128,3 +132,25 @@ with st.form("form 2",clear_on_submit=True):
             st.warning("please fill the above fields")
         else:
             st.success("submitted successfully")
+
+opt=st.sidebar.radio("Select any graph", options=("Line","Bar","H-Bar"))
+if opt=="Line":
+    st.markdown("<h1 style='text-align:center;'>Line Chart</h1>", unsafe_allow_html=True)
+    fig=plt.figure()
+    plt.style.use('ggplot')
+    plt.plot(x, np.sin(x))
+    plt.plot(x,np.cos(x),'--')
+    st.write(fig)
+
+elif opt=="Bar":
+    st.markdown("<h1 style='text-align:center;'>Bar Chart</h1>", unsafe_allow_html=True)
+    fig=plt.figure()
+    plt.style.use('ggplot')
+    plt.bar(bar_x,bar_x*10)
+    st.write(fig)
+else:
+    st.markdown("<h1 style='text-align:center;'>H-Bar Chart</h1>", unsafe_allow_html=True)
+    fig=plt.figure()
+    plt.style.use('ggplot')
+    plt.barh(bar_x,bar_x*10)
+    st.write(fig)
