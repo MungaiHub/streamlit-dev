41f02983af7961e67bdd1e81ac038dee2ad918cb
mungaiHub <amosmungai085@gmail.com>
2025-09-21 16:47:07 +0300

    add copy functionality

--- url_shortener.py
+++ url_shortener.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import pyshorteners as pyst
 shortener=pyst.Shortener()
+import pyperclip
+def copy():
+    pyperclip.copy(shortened_url)
+
 st.markdown("<h1 style='text-align:center;'>URL Shortener<h1>", unsafe_allow_html=True)
 form=st.form("name")
 url=form.text_input("URL HERE")
 short_btn=form.form_submit_button("Shorten")
 if short_btn:
     shortened_url=shortener.tinyurl.short(url)
-    st.write(shortened_url)
+    st.markdown("<h3>Shortened url<h3>", unsafe_allow_html=True)
+    st.markdown(f"<h6 style='text-align:center;'>{shortened_url}<h6>", unsafe_allow_html=True)
+    st.button("Copy", on_click=copy)
