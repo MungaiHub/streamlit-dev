ab6b15a4c6561f92181c21577880fb6d1ba51ba8
mungaiHub <amosmungai085@gmail.com>
2025-09-25 16:57:32 +0300

    initial commit with Streamlit audio-to-text project setup

---
+++ audio_to_text.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from pydub import AudioSegment, silence
+st.markdown("<h1 style='text-align:center;'>Audio To Text Converter<h1>", unsafe_allow_html=True)
+st.markdown("---")
+audio=st.file_uploader("upload your audio file",type=["mp3","wav"])
+if audio:
+    audio_segment=AudioSegment.from_file(audio)
+    chunks=silence.split_on_silence(audio_segment,min_silence_len=500,silence_thresh=audio_segment.dBFS-20,keep_silence=100)
+    st.write(chunks)
