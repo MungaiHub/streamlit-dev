2d2353642b97598bd18085ddca045f767330c127
mungaiHub <amosmungai085@gmail.com>
2025-09-27 16:14:03 +0300

    continution of innitial commit

--- data_visualizer.py
+++ data_visualizer.py
@@ -1,5 +1,11 @@
 import streamlit as st
-
+import pandas as pd
+def date_converter (date_col):
+    result=list()
+    values=date_col.values
+    for value in values:
+        result.append(str(value).split("T")[0])
+    return result
 st.markdown("<h1 style='text-align:center;'>Data Visualizer<h1>", unsafe_allow_html=True)
 st.markdown("---")
 files_names=list()
@@ -11,5 +17,10 @@ selected_files=st.multiselect("Select fils", options=files_names)
 if selected_files:
     option=st.radio("Select entity against date", options=["None","First Name", "Last Name","Gender", "Country", "Age","Date", "Id"]) 
     if option != "None":
-       st.write(option) 
+       for file in files:
+           if file.name in selected_files:
+               sample_data=pd.read_excel(file,index_col=0)
+               dates=date_converter(sample_data["Date"])
+               st.write(dates)
+               
 
